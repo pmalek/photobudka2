6c93a47c9750baf7318e7930223684ee026ecf4d
Patryk Małek <malekpatryk@gmail.com>
2016-07-02 17:16:10 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project and add stage changes
-hugo && git add -A
-
-# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
+# Build the project and add stage changes
+hugo -t hugo-agency-theme && \
+git add -A && \
 git commit -m "$msg" && \
 git push origin master && \
 git subtree push --prefix=public $(git config --get remote.origin.url) gh-pages
